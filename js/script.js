125b2c2ed6aaa27ef9cbd735ad914955fef81192
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-08 16:37:49 +0200

    [add] pseudo codice

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* */
+//Inizio del programma
+/*
+// MILESTONE 1: Implementazione estetica
+1. Chiedi all'utente di inserire il numero di chilometri da percorrere.
+2. Chiedi all'utente di inserire l'età del passaggero.
+3. Calcoliamo il prezzo del biglietto.
+4. Se l'età è inferiore o uguale a 18, appliciamo uno sconto del 20% al prezzo.
+5. Se l'età è superiore o uguale a 65, applica uno sconto del 40% al prezzo.
+6. Stampa il prezzo finale formattato con massimo due decimali.
+*/
+
+/*
+//MILESTONE 2: Implementazione con form in pagina
+7. Creiamo un forma HTML con due campi di input (chilometri e età) e un pulsante "Calcola."
+8. Creiamo un elemento in pagina per visualizzare il risulatato.
+9. Aggiungiamo un evento al pulsante "Calcola" per eseguire il calcolo quando viene premuto.
+10. Mostriamo il risultato in una parte dedicata della pagina, formattando il prezzo con massimo due decimali.
+11. Mostriamo anche un recap dei dati inseriti dall'utente.
+//Fine del programma
+*/
+/* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* *//* */
